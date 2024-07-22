de588d884ae5522ae7b3e264dd68da7210b8c2e2
Andrew Finogenov <avfinogenov@gmail.com>
2024-07-22 18:40:34 +0300

    third time is the charm

--- main.cpp
+++ main.cpp
@@ -4,18 +4,52 @@
 
 void firstMethod(int givenNumber);
 void secondMethod(int givenNumber);
+void thirdMethod(int givenNumber);
+
 
 int main()
 {
 	srand(time(NULL));
 
 	const int MAX_GIVEN_NUMBER = 4000;
-	int givenNumber = rand() % MAX_GIVEN_NUMBER;
+	int givenNumber = rand() % INT_MAX;
 	firstMethod(givenNumber);
 	secondMethod(givenNumber);
-
+	thirdMethod(givenNumber);
 
 }
+
+void thirdMethod(int givenNumber)
+{
+
+	int givenNumberForOutput = givenNumber;
+	int operationsCount = 0;
+
+	while (givenNumber > 1)
+	{
+		if (givenNumber % 16 == 11 || givenNumber % 16 == 15 || givenNumber % 16 == 7)
+		{
+			givenNumber++;
+			operationsCount++;
+			continue;
+		}
+		if (givenNumber % 2 == 1)
+		{
+			givenNumber--;
+			operationsCount++;
+			continue;
+		}
+		givenNumber /= 2;
+		operationsCount++;
+
+	}
+	std::cout << "third method\n";
+	std::cout << "given number: " + std::to_string(givenNumberForOutput) + "; operations count: " + std::to_string(operationsCount);
+	std::cout << "\n";
+
+}
+
+
 
 int getSquareCompare(int givenNumber, int baseCompare)
 {
@@ -60,7 +94,7 @@ void secondMethod(int givenNumber)
 	}
 	std::cout << "second method\n";
 	std::cout << "given number: " + std::to_string(givenNumberForOutput) + "; operations count: " + std::to_string(operationsCount);
-
+	std::cout << "\n";
 
 }
 
@@ -84,6 +118,6 @@ void firstMethod(int givenNumber)
 	}
 	std::cout << "first method\n";
 	std::cout << "given number: " + std::to_string(givenNumberForOutput) + "; operations count: " + std::to_string(operationsCount);
-
+	std::cout << "\n";
 
 }
